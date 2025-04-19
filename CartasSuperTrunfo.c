43d6615efeffcc631d9d689197ac2c8265e7f3a3
ferfix1910 <ferfix@live.com>
2025-04-19 01:36:39 +0000

    Teste Final

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,41 +2,41 @@
 
     int main() {
     
-    printf("Super Trunfo!");
+    printf("Super Trunfo! \n");
 
    // Todas as variáveis com o final 01 correspondem a primeira carta.//
    // Todas as variáveis com o final 02 correspondem a segunda carta.//
    // A variável Pontos é correspondentende aos pontos turisticos.//
 
-    char Estado01[50];
-    char Código01 [50];
-    char Nome01 [50];
-    int População01;
+    char Estado01[100];
+    char Codigo01 [100];
+    char Nome01 [100];
+    int Populacao01;
     float Area01;
     float PIB01;
     int Pontos01;
     
-    char Estado02[50];
-    char Código02 [50];
-    char Nome02 [50];
-    int População02;
+    char Estado02[100];
+    char Codigo02 [100];
+    char Nome02 [100];
+    int Populacao02;
     float Area02;
     float PIB02;
     int Pontos02;
     
-   printf("A seguir coloque as informações dos dados da carta 01");
+   printf("A seguir coloque as informações dos dados da carta 01: \n");
 
    printf("Digite o nome do Estado01: \n");
    scanf("%s", &Estado01);
    
    printf("Digite um código para carta 01: \n");
-   scanf("%s", &Código01);
+   scanf("%s", &Codigo01);
    
    printf("Digite a Cidade: \n");
    scanf("%s", &Nome01);
 
    printf("Digite a população: \n");
-   scanf("%d", &População01);
+   scanf("%d", &Populacao01);
 
    printf("Digite a Area em Km² : \n");
    scanf("%f", &Area01);
@@ -47,19 +47,19 @@
    printf("Digite a quantidade de pontos Turisticos: \n");
    scanf("%d",&Pontos01);
 
-   printf("A seguir coloque as informações dos dados da carta 02");
+   printf("A seguir coloque as informações dos dados da carta 02: \n");
     
    printf("Digite o nome do Estado02: \n");
    scanf("%s", &Estado02);
 
    printf("Digite o código para a carta 02: \n");
-   scanf("%s",&Código02);
+   scanf("%s",&Codigo02);
 
    printf("Digite a cidade: \n");
    scanf("%s", &Nome02);
 
    printf("Digite a população: \n");
-   scanf("%d", &População02);
+   scanf("%d", &Populacao02);
 
    printf("Digite a Area por Km²: \n");
    scanf("%f", &Area02);
@@ -71,14 +71,12 @@
    scanf("%d", &Pontos02);
 
 
-   
 
 
-
-
-
-
-    return 0;
+  
+  
+  
+   return 0;
 
 
 
